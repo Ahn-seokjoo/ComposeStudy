6ae20c27c7018291da28c03e594e3d77a7c1504e
ahn seokjoo <tjrwn0716@naver.com>
2022-06-02 02:03:12 +0900

    week1 : Column, Row 사용해보기

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -25,22 +28,43 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// Column 으로, 아이템 전체 요소를 Column 으로
 @Composable
-private fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting(name = "Android")
-
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
+// item 한개
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                /** weight를 줌으로써 공간을 채우는 동시에 다른 요소를 밀어내서, 끝으로 보냄
+                 * fillMaxWidth를 안써도 됨. -> 중복됨
+                 */
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+
+            OutlinedButton(onClick = { }) {
+                Text("Show more")
+            }
+        }
+
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
