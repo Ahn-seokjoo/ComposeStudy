d744819e9c8f69b0fd0c71ee86fcc25efe88d01a
ahn seokjoo <tjrwn0716@naver.com>
2022-06-02 04:02:48 +0900

    week1 : Text 추가 및 자잘한 설정 추가

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,16 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -57,53 +60,67 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
     }
 }
 
+@Composable
+private fun Greeting(name: String) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
 // item 한개
 @Composable
-fun Greeting(name: String) {
+fun CardContent(name: String) {
     /**
     Compose는 Ui 안의 data가 변경되면 composable function을 호출하면서 데이터를 변경하는데
     이를 recomposition이라고 합니다. 이때마다 변수로 선언하면 변수가 계속 false로 불림
     이를 방지하기 위해 이전의 데이터를 유지하는 remember를 사용하면 reset 되지 않고 데이터를 유지시킬 수 있다!
      */
-    val expanded = remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
-    val extraPadding by animateDpAsState(
-        if (expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                /** weight를 줌으로써 공간을 채우는 동시에 다른 요소를 밀어내서, 끝으로 보냄
-                 * fillMaxWidth를 안써도 됨. -> 중복됨
-                 */
-                modifier = Modifier
-                    .weight(1f)
-                    /**
-                     * padding 바텀으로 주기
-                     */
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello, ")
-                Text(
-                    text = name, style = MaterialTheme.typography.h4.copy(
-                        fontWeight = FontWeight.Bold
-                    )
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
                 )
-            }
-
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if (expanded.value) "Show less" else "Show more")
+            )
+    ) {
+        Column(
+            /** weight를 줌으로써 공간을 채우는 동시에 다른 요소를 밀어내서, 끝으로 보냄
+             * fillMaxWidth를 안써도 됨. -> 중복됨
+             */
+            modifier = Modifier
+                .weight(1f)
+                /**
+                 * padding 바텀으로 주기
+                 */
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name, style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
         }
 
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = (if (expanded) "Show more" else "Show less")
+            )
+        }
     }
 }
 
